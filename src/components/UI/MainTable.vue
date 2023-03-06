fab23fca925dce641819f2d238b7ae9d52b1bed0
Julio <julio_ornellas@yahoo.com.br>
2023-03-06 16:03:46 -0300

    init: Starting to build te layout

---
+++ src/components/UI/MainTable.vue
@@ -0,0 +1,102 @@
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+<template>
+  <table class="w-full bg-white dark:bg-slate-800 text-sm shadow-sm">
+    <thead>
+      <tr>
+        <th
+          class="border-b border-gray-900 dark:text-slate-200 dark:border-gray-600 p-2"
+        >
+          DATA SENT
+        </th>
+        <th
+          class="border-b border-gray-900 dark:text-slate-200 dark:border-gray-600 p-2"
+        >
+          COMPANY
+        </th>
+        <th
+          class="border-b border-gray-900 dark:text-slate-200 dark:border-gray-600 p-2"
+        >
+          FIX
+        </th>
+        <th
+          class="border-b border-gray-900 dark:text-slate-200 dark:border-gray-600 p-2"
+        >
+          FRN
+        </th>
+        <th
+          class="border-b border-gray-900 dark:text-slate-200 dark:border-gray-600 p-2"
+        >
+          FIX
+        </th>
+        <th
+          class="border-b border-gray-900 dark:text-slate-200 dark:border-gray-600 p-2"
+        >
+          FRN
+        </th>
+        <th
+          class="border-b border-gray-900 dark:text-slate-200 dark:border-gray-600 p-2"
+        >
+          FIX
+        </th>
+        <th
+          class="border-b border-gray-900 dark:text-slate-200 dark:border-gray-600 p-2"
+        >
+          FRN
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="hover:bg-gray-100" v-for="(item, index) in items" :key="index">
+        <td
+          class="border-b border-gray-300 dark:border-gray-700 p-2 dark:text-slate-400"
+        >
+          {{ item.DateSent }}
+        </td>
+        <td
+          class="border-b border-gray-300 dark:border-gray-700 p-2 dark:text-slate-400"
+        >
+          {{ item.Company }}
+        </td>
+        <td
+          class="border-b border-gray-300 dark:border-gray-700 p-2 dark:text-slate-400"
+        >
+          latitude
+        </td>
+        <td
+          class="border-b border-gray-300 dark:border-gray-700 p-2 dark:text-slate-400"
+        >
+          longitude
+        </td>
+        <td
+          class="border-b border-gray-300 dark:border-gray-700 p-2 dark:text-slate-400"
+        >
+          longitude
+        </td>
+        <td
+          class="border-b border-gray-300 dark:border-gray-700 p-2 dark:text-slate-400"
+        >
+          longitude
+        </td>
+        <td
+          class="border-b border-gray-300 dark:border-gray-700 p-2 dark:text-slate-400"
+        >
+          longitude
+        </td>
+        <td
+          class="border-b border-gray-300 dark:border-gray-700 p-2 dark:text-slate-400"
+        >
+          longitude
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,66 @@
+<script>
+// @ is an alias to /src
+import { useStore } from "vuex";
+import MainTable from "@/components/UI/MainTable.vue";
+import data from "@/data/data.json";
+export default {
+  name: "LandingPage",
+  components: {
+    MainTable,
+  },
+  setup() {
+    const store = useStore();
+    //Usar essa pagina como principal e inserir dados no VUEX aqui e buscar direto na tabela dos items
+    return {
+      title: "iPrice Challenge",
+      items: store.state.items,
+      fromJson: data.Items,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="mx-8">
+    <div class="mt-3">
+      <p class="text-2xl font-bold mb-3">{{ title }}</p>
+      <div class="my-2 flex justify-start">
+        <div>
+          <span class="p-2 border rounded-l-md text-blue-700 text-xs">USD</span>
+          <span class="p-2 border text-blue-700 text-xs">EUR</span>
+          <span class="p-2 border rounded-r-md text-blue-700 text-xs">CAD</span>
+        </div>
+        <div class="mx-4">
+          <span class="p-2 border rounded-l-md text-blue-700 text-xs"
+            >5 YRS</span
+          >
+          <span class="p-2 border text-blue-700 text-xs">10 YRS</span>
+          <span class="p-2 border rounded-r-md text-blue-700 text-xs"
+            >40 YRS</span
+          >
+        </div>
+        <div>
+          <span class="p-2 border rounded-l-md text-blue-700 text-xs"
+            >Spread</span
+          >
+          <span class="p-2 border text-blue-700 text-xs">Yield</span>
+          <span class="p-2 border rounded-r-md text-blue-700 text-xs"
+            >3MLSpread</span
+          >
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-start my-4">
+      <input
+        type="text"
+        class="border px-2 text-sm rounded"
+        placeholder="Filter by compan name"
+      />
+    </div>
+
+    <div>
+      <MainTable :items="fromJson"></MainTable>
+    </div>
+  </div>
+</template>
