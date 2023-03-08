35160458a14de89b986d934fd3792a56281c6dbf
Julio <julio_ornellas@yahoo.com.br>
2023-03-08 14:11:45 -0300

    refactor: Format date, adjust year

--- src/plugins/date-filter.js
+++ src/plugins/date-filter.js
@@ -19,7 +19,7 @@ export const dateFilter = (value) => {
 
 function formatDate(inputDate) {
   const date = new Date(inputDate);
-  const year = date.getFullYear();
+  const year = date.getFullYear().toString().slice(-2);
   const month = date.getMonth();
   const day = date.getDate() + 1;
   const formattedDate = `${day}-${months[month]}-${year}`;
